b3b012b7267955284ef41d1768a896ea13e6ca33
ValaAntal <valaczkaiocsi68@gmail.com>
2025-01-16 17:37:12 +0100

    Hibák ki javítása

--- apps/api/controllers/user.controller.js
+++ apps/api/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import express from 'express';
+
 import { PrismaClient } from '@prisma/client';
-import prisma from 'PrismaClient';
+const prisma = new PrismaClient();
+const app = express();
 
 const createUser = async (req, res) => {};
 
@@ -9,3 +11,10 @@ const updateUser = async (req, res) => {};
 const getUser = async (req, res) => {};
 
 const deleteUser = async (req, res) => {};
+
+module.exports = {
+  updateUser,
+  createUser,
+  getUser,
+  deleteUser,
+};
